f78db8a2163ec3590b85750ca60cdd7696260fc1
Greg <garnold.805@gmail.com>
2022-04-15 15:42:20 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData( name, email ) {
+    // send data
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+    // handle response
+      .then( function ( response ) {
+        return response.json()
+      } )
+    // retrieves the new id value and appends it to the DOM
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+    // handle errors
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
